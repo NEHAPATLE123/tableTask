687d8ee125138deccef0701ed3fb379809aad7a1
NEHAPATLE123 <117140135+NEHAPATLE123@users.noreply.github.com>
2023-04-15 13:33:50 +0530

    jh
    h

---
+++ src/app/shared/component/model/std.ts
@@ -0,0 +1,11 @@
+
+export interface Istd {
+    name: string;
+    country: string;
+    role: string;
+    age: number;
+    matchesPlayed: number;
+    totalRuns: number;
+    totalWickets: number;
+
+}
